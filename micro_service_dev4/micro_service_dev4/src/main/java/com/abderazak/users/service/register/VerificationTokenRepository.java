65ce930e1aeeee84736252292dd57991ee112354
dimensions <127557181+Dhiaturki@users.noreply.github.com>
2024-12-08 13:01:56 +0100

    f1

--- micro_service_dev4/micro_service_dev4/src/main/java/com/abderazak/users/service/register/VerificationTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abderazak.users.service.register;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VerificationTokenRepository extends
-JpaRepository<VerificationToken, Long> {
- VerificationToken findByToken(String token);
-}
--- micro_service_dev4/micro_service_dev4/src/main/java/com/abderazak/users/util/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package com.abderazak.users.util;
-
-public interface EmailSender {
-	void sendEmail(String toEmail, String body);
-}
